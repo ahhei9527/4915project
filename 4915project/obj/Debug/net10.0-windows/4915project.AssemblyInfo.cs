9849fde699583201032cd7752a6d46a8c4930776
ahhei9527 <hocheukhei821@gmail.com>
2026-05-29 17:28:04 +0800

    2.1.1

--- 4915project/obj/Debug/net10.0-windows/4915project.AssemblyInfo.cs
+++ 4915project/obj/Debug/net10.0-windows/4915project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4915project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937a3e5f31894c17ff844867082bb9bf9c9f9afe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4e899f6fd38465dcfbf21dff69a9687d01f55a")]
 [assembly: System.Reflection.AssemblyProductAttribute("4915project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4915project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
