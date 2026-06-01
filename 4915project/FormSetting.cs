f0a2a6c544cb77635ce5e901860230cb64f37746
ahhei9527 <hocheukhei821@gmail.com>
2026-06-02 00:22:12 +0800

    4.0

--- 4915project/FormSetting.cs
+++ 4915project/FormSetting.cs
@@ -1,25 +1,371 @@
-﻿using System;
+﻿using _4915project;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ITP4915M
 {
     public partial class FormSetting : Form
     {
+        string constring = "server=localhost;user id=root;password=;database=4915";
         public FormSetting()
         {
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void FormSetting_Load(object sender, EventArgs e)
         {
-
+            LoadCompany();
+            LoadUser();
+            //LoadCustomer();
+            cmbComLan.Items.AddRange(new string[] { "Chinese", "English" });
+            cmbComWH.Items.AddRange(new string[] { "WH-A-12-03", "WH-A-12-04", "WH-B-05-01", "WH-C-08-02" });
+            cmbComCurr.Items.AddRange(new string[] { "HKD", "USD", "CNY" });
+            cmbRole.Items.AddRange(new string[] { "Admin", "Staff" });
+            cmbDepartment.Items.AddRange(new string[] { "Sales", "Production", "Warehouse", "Design", "After Sales", "Logistics" });
+            tbPassword.Enabled = false;
+            cmbRole.Enabled = false;
+            cmbDepartment.Enabled = false;
+            tbPosit.Enabled = false;
         }
+
+        private void LoadCompany()
+        {
+            // Code to load company data into the form
+            string query = "SELECT Companyname FROM company";
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, con))
+                {
+                    try
+                    {
+                        con.Open();
+                        MySqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            cmbCompanyName.Items.Add(reader["CompanyName"].ToString());
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error loading company data: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void LoadUser()
+        {
+            // Code to load user data into the form
+            string query = "SELECT Name FROM user";
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, con))
+                {
+                    try
+                    {
+                        con.Open();
+                        MySqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            cmbUserName.Items.Add(reader["Name"].ToString());
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error loading company data: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void cmbCompanyName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadCompanyData();
+        }
+
+        private void btAddCom_Click(object sender, EventArgs e)
+        {
+            addCompany addCompany = new addCompany();
+            addCompany.Show();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonResetDefault_Click(object sender, EventArgs e)
+        {
+            LoadCompanyData();
+        }
+
+        private void LoadCompanyData()
+        {
+            // Code to load default company data into the form
+            string companyQuery = "SELECT * FROM company WHERE CompanyName = @CompanyName";
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(companyQuery, con))
+                {
+                    cmd.Parameters.AddWithValue("@CompanyName", cmbCompanyName.SelectedItem?.ToString());
+                    try
+                    {
+                        con.Open();
+                        MySqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            cmbComLan.SelectedItem = reader["launage"].ToString();
+                            cmbComWH.SelectedItem = reader["warehouse"].ToString();
+                            cmbComCurr.SelectedItem = reader["currency"].ToString();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error loading company details: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            string comQuery = "UPDATE company SET " +
+                "launage = @Language, " +
+                "warehouse = @Warehouse, " +
+                "currency = @Currency " +
+                "WHERE CompanyName = @CompanyName";
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(comQuery, con))
+                {
+                    cmd.Parameters.AddWithValue("@Language", cmbComLan.SelectedItem?.ToString());
+                    cmd.Parameters.AddWithValue("@Warehouse", cmbComWH.SelectedItem?.ToString());
+                    cmd.Parameters.AddWithValue("@Currency", cmbComCurr.SelectedItem?.ToString());
+                    cmd.Parameters.AddWithValue("@CompanyName", cmbCompanyName.SelectedItem?.ToString());
+                    try
+                    {
+                        con.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Company settings updated successfully.");
+                            UpdateCompanySettingsAudit();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No changes were made to the company settings.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error saving company settings: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void UpdateCompanySettingsAudit()
+        {
+            try
+            {
+                CurrentUser.AddInward(CurrentUser.UserID,
+                    CurrentUser.Username, CurrentUser.Role,
+                    CurrentUser.Email);
+
+                AuditHelper.Log(
+                    tableName: "company",
+                    recordId: cmbCompanyName.SelectedItem?.ToString(),
+                    action: "Update  company",
+                    userId: CurrentUser.UserID,
+                    newValues: new
+                    {
+                        Language = cmbComLan.SelectedItem?.ToString(),
+                        Warehouse = cmbComWH.SelectedItem?.ToString(),
+                        Currency = cmbComCurr.SelectedItem?.ToString(),
+                        CompanyName = cmbCompanyName.SelectedItem?.ToString()
+                    }
+                    );
+            }
+            catch (Exception ex)
+            {
+                // 日誌記錄失敗不應阻擋用戶，僅做提示
+                Console.WriteLine("Audit Log failed: " + ex.Message);
+            }
+        }
+
+        private void cmbUserName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadUserData();
+        }
+
+        private void LoadUserData()
+        {
+            // Code to load default user data into the form
+            string userQuery = "SELECT * FROM user WHERE Name = @Name";
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(userQuery, con))
+                {
+                    cmd.Parameters.AddWithValue("@Name", cmbUserName.SelectedItem?.ToString());
+                    try
+                    {
+                        con.Open();
+                        MySqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            tbPassword.Text = reader["password"].ToString();
+                            cmbRole.SelectedItem = reader["Role"].ToString();
+                            cmbDepartment.SelectedItem = reader["Department"].ToString();
+                            tbPosit.Text = reader["position"].ToString();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error loading user details: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            LoadUserData();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                tbPassword.Enabled = true;
+            }
+            else
+            {
+                tbPassword.Enabled = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                cmbRole.Enabled = true;
+                cmbDepartment.Enabled = true;
+                tbPosit.Enabled = true;
+            }
+            else
+            {
+                cmbRole.Enabled = false;
+                cmbDepartment.Enabled = false;
+                tbPosit.Enabled = false;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string userQuery = "UPDATE user SET " +
+                "password = @password, " +
+                "Role = @Role, " +
+                "position = @position, " +
+                "Department = Department " +
+                "WHERE Name = @Name";
+            using (MySqlConnection con = new MySqlConnection(constring))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(userQuery, con))
+                {
+                    cmd.Parameters.AddWithValue("@password", tbPassword.Text);
+                    cmd.Parameters.AddWithValue("@Role", cmbRole.SelectedItem?.ToString());
+                    cmd.Parameters.AddWithValue("@position", tbPosit.Text.ToString());
+                    cmd.Parameters.AddWithValue("@Department", cmbDepartment.SelectedItem?.ToString());
+                    cmd.Parameters.AddWithValue("@Name", cmbUserName.SelectedItem?.ToString());
+                    try
+                    {
+                        con.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Company settings updated successfully.");
+                            UpdateUserSettingsAudit();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No changes were made to the company settings.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error saving company settings: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void UpdateUserSettingsAudit()
+        {
+            try
+            {
+                CurrentUser.AddInward(CurrentUser.UserID,
+                    CurrentUser.Username, CurrentUser.Role,
+                    CurrentUser.Email);
+                AuditHelper.Log(
+                    tableName: "user",
+                    recordId: cmbUserName.SelectedItem?.ToString(),
+                    action: "Update user",
+                    userId: CurrentUser.UserID,
+                    newValues: new
+                    {
+                        Role = cmbRole.SelectedItem?.ToString(),
+                        position = tbPosit.Text.ToString(),
+                        Department = cmbDepartment.SelectedItem?.ToString()
+                    }
+                    );
+            }
+            catch (Exception ex)
+            {
+                // 日誌記錄失敗不應阻擋用戶，僅做提示
+                Console.WriteLine("Audit Log failed: " + ex.Message);
+            }
+        }
+
+
+
+        //private void LoadCustomer()
+        //{
+        //    // Code to load customer data into the form
+        //    string query = "SELECT Companyname FROM company";
+        //    using (MySqlConnection con = new MySqlConnection(constring))
+        //    {
+        //        using (MySqlCommand cmd = new MySqlCommand(query, con))
+        //        {
+        //            try
+        //            {
+        //                con.Open();
+        //                MySqlDataReader reader = cmd.ExecuteReader();
+        //                while (reader.Read())
+        //                {
+        //                    cmbCompanyName.Items.Add(reader["CompanyName"].ToString());
+        //                }
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                MessageBox.Show("Error loading company data: " + ex.Message);
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
---
+++ 4915project/addCompany.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace _4915project
+{
+    partial class addCompany
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel2 = new Panel();
+            tbComName = new TextBox();
+            buttonCancel = new Button();
+            buttonSave = new Button();
+            buttonResetDefault = new Button();
+            labelCompanyName = new Label();
+            cmbComCurr = new ComboBox();
+            labelDefaultLanguage = new Label();
+            labelDefaultWarehouse = new Label();
+            labelDefaultCurrency = new Label();
+            cmbComWH = new ComboBox();
+            cmbComLan = new ComboBox();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(tbComName);
+            panel2.Controls.Add(buttonCancel);
+            panel2.Controls.Add(buttonSave);
+            panel2.Controls.Add(buttonResetDefault);
+            panel2.Controls.Add(labelCompanyName);
+            panel2.Controls.Add(cmbComCurr);
+            panel2.Controls.Add(labelDefaultLanguage);
+            panel2.Controls.Add(labelDefaultWarehouse);
+            panel2.Controls.Add(labelDefaultCurrency);
+            panel2.Controls.Add(cmbComWH);
+            panel2.Controls.Add(cmbComLan);
+            panel2.Location = new Point(2, 11);
+            panel2.Margin = new Padding(2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(518, 220);
+            panel2.TabIndex = 13;
+            // 
+            // tbComName
+            // 
+            tbComName.Location = new Point(129, 14);
+            tbComName.Name = "tbComName";
+            tbComName.Size = new Size(335, 23);
+            tbComName.TabIndex = 16;
+            // 
+            // buttonCancel
+            // 
+            buttonCancel.Location = new Point(434, 146);
+            buttonCancel.Margin = new Padding(2);
+            buttonCancel.Name = "buttonCancel";
+            buttonCancel.Size = new Size(71, 22);
+            buttonCancel.TabIndex = 15;
+            buttonCancel.Text = "Cancel";
+            buttonCancel.UseVisualStyleBackColor = true;
+            buttonCancel.Click += buttonCancel_Click;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(327, 146);
+            buttonSave.Margin = new Padding(2);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(71, 22);
+            buttonSave.TabIndex = 14;
+            buttonSave.Text = "Save";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += buttonSave_Click;
+            // 
+            // buttonResetDefault
+            // 
+            buttonResetDefault.Location = new Point(195, 146);
+            buttonResetDefault.Margin = new Padding(2);
+            buttonResetDefault.Name = "buttonResetDefault";
+            buttonResetDefault.Size = new Size(93, 22);
+            buttonResetDefault.TabIndex = 13;
+            buttonResetDefault.TabStop = false;
+            buttonResetDefault.Text = "Reset Default";
+            buttonResetDefault.UseVisualStyleBackColor = true;
+            buttonResetDefault.Click += buttonResetDefault_Click;
+            // 
+            // labelCompanyName
+            // 
+            labelCompanyName.AutoSize = true;
+            labelCompanyName.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            labelCompanyName.Location = new Point(21, 16);
+            labelCompanyName.Margin = new Padding(2, 0, 2, 0);
+            labelCompanyName.Name = "labelCompanyName";
+            labelCompanyName.Size = new Size(103, 15);
+            labelCompanyName.TabIndex = 0;
+            labelCompanyName.Text = "Company Name:";
+            // 
+            // cmbComCurr
+            // 
+            cmbComCurr.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            cmbComCurr.FormattingEnabled = true;
+            cmbComCurr.Items.AddRange(new object[] { "HKD" });
+            cmbComCurr.Location = new Point(129, 101);
+            cmbComCurr.Margin = new Padding(2);
+            cmbComCurr.Name = "cmbComCurr";
+            cmbComCurr.Size = new Size(102, 23);
+            cmbComCurr.TabIndex = 9;
+            // 
+            // labelDefaultLanguage
+            // 
+            labelDefaultLanguage.AutoSize = true;
+            labelDefaultLanguage.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            labelDefaultLanguage.Location = new Point(13, 44);
+            labelDefaultLanguage.Margin = new Padding(2, 0, 2, 0);
+            labelDefaultLanguage.Name = "labelDefaultLanguage";
+            labelDefaultLanguage.Size = new Size(111, 15);
+            labelDefaultLanguage.TabIndex = 1;
+            labelDefaultLanguage.Text = "Default Language:";
+            // 
+            // labelDefaultWarehouse
+            // 
+            labelDefaultWarehouse.AutoSize = true;
+            labelDefaultWarehouse.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            labelDefaultWarehouse.Location = new Point(4, 70);
+            labelDefaultWarehouse.Margin = new Padding(2, 0, 2, 0);
+            labelDefaultWarehouse.Name = "labelDefaultWarehouse";
+            labelDefaultWarehouse.Size = new Size(118, 15);
+            labelDefaultWarehouse.TabIndex = 4;
+            labelDefaultWarehouse.Text = "Default Warehouse:";
+            // 
+            // labelDefaultCurrency
+            // 
+            labelDefaultCurrency.AutoSize = true;
+            labelDefaultCurrency.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            labelDefaultCurrency.Location = new Point(4, 101);
+            labelDefaultCurrency.Margin = new Padding(2, 0, 2, 0);
+            labelDefaultCurrency.Name = "labelDefaultCurrency";
+            labelDefaultCurrency.Size = new Size(103, 15);
+            labelDefaultCurrency.TabIndex = 3;
+            labelDefaultCurrency.Text = "Default Currency:";
+            // 
+            // cmbComWH
+            // 
+            cmbComWH.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            cmbComWH.FormattingEnabled = true;
+            cmbComWH.Items.AddRange(new object[] { "WH-01" });
+            cmbComWH.Location = new Point(129, 74);
+            cmbComWH.Margin = new Padding(2);
+            cmbComWH.Name = "cmbComWH";
+            cmbComWH.Size = new Size(102, 23);
+            cmbComWH.TabIndex = 7;
+            // 
+            // cmbComLan
+            // 
+            cmbComLan.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            cmbComLan.FormattingEnabled = true;
+            cmbComLan.Items.AddRange(new object[] { "English", "Chinese" });
+            cmbComLan.Location = new Point(129, 42);
+            cmbComLan.Margin = new Padding(2);
+            cmbComLan.Name = "cmbComLan";
+            cmbComLan.Size = new Size(102, 23);
+            cmbComLan.TabIndex = 6;
+            // 
+            // addCompany
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(528, 237);
+            Controls.Add(panel2);
+            Name = "addCompany";
+            Text = "addCompany";
+            Load += addCompany_Load;
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private TextBox tbComName;
+        private Button buttonCancel;
+        private Button buttonSave;
+        private Button buttonResetDefault;
+        private Label labelCompanyName;
+        private ComboBox cmbComCurr;
+        private Label labelDefaultLanguage;
+        private Label labelDefaultWarehouse;
+        private Label labelDefaultCurrency;
+        private ComboBox cmbComWH;
+        private ComboBox cmbComLan;
+    }
+}
